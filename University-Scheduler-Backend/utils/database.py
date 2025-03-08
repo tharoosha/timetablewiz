a9102f3b6caa234e6c4322813cef511b37937807
tharoosha <tharooshavihidun@gmail.com>
2025-03-08 11:04:15 +0530

    finalizing database connection

--- University-Scheduler-Backend/utils/database.py
+++ University-Scheduler-Backend/utils/database.py
@@ -1,7 +1,12 @@
 from pymongo import MongoClient
+import os
+from dotenv import load_dotenv
 
-# MONGODB_URI: str = "mongodb+srv://wkmswanthra:wkmswanthra@test-bed.dtgmi5a.mongodb.net/?retryWrites=true&w=majority&appName=test-bed"
-MONGODB_URI: str = "mongodb+srv://tharooshavihidun:<db_password>@cluster0.fndwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+load_dotenv()
+
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+
+MONGODB_URI: str = f"mongodb+srv://tharooshavihidun:{DATABASE_PASSWORD}@cluster0.fndwc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 client = MongoClient(MONGODB_URI)
 db = client["time_table_whiz"]
